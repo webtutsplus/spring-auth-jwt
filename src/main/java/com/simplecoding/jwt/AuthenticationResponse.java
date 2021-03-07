a1ebc2f0763f6a408a132b82f1be60d0af95a496
nmadhab <nilmadhab1994@booking.com>
2021-03-07 19:58:17 +0530

    A basic JWT auth implemented following the tutorial https://www.youtube.com/watch?v=X80nJ5T7YpE&t=911s
    Features
    1. user hardcoded
    2. JWT token generated

---
+++ src/main/java/com/simplecoding/jwt/AuthenticationResponse.java
@@ -0,0 +1,18 @@
+package com.simplecoding.jwt;
+
+public class AuthenticationResponse {
+
+    private String jwt;
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+}
---
+++ src/main/java/com/simplecoding/jwt/SecurityConfigurer.java
@@ -0,0 +1,45 @@
+package com.simplecoding.jwt;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private MyUserDetailsService myUserDetailService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+        auth.userDetailsService(myUserDetailService);
+    }
+
+    @Override
+    public void configure(HttpSecurity security) throws Exception {
+        security.csrf().disable()
+                .authorizeRequests().antMatchers("/authenticate").permitAll()
+                .anyRequest().authenticated();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+}
